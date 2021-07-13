bac73ec7fb1886514e38894cbf38d8f23adce4ad
glemin5011 <matejpale@gmail.com>
2021-07-13 18:20:12 +0200

    removed base currency from currency list

--- src/CurrencyLists.js
+++ src/CurrencyLists.js
@@ -4,15 +4,17 @@ const CurrencyLists = (props) => {
   const { i, codes, rate, baseCurrency } = props;
   return (
     <React.Fragment>
-      <li key={i}>
-        <p>
-          <img
-            src={require(`./country-logos/${codes}.png`).default}
-            className="currency-lists-icon d-inline mx-2"
-          ></img>
-          {codes} : {rate[codes]}
-        </p>
-      </li>
+      {codes !== baseCurrency ? (
+        <li key={i}>
+          <p>
+            <img
+              src={require(`./country-logos/${codes}.png`).default}
+              className="currency-lists-icon d-inline mx-2"
+            ></img>
+            {codes} : {rate[codes]}
+          </p>
+        </li>
+      ) : null}
     </React.Fragment>
   );
 };
